3b13d5b8149f4a5309b0bb61eb0222f0c17501e6
CSE2023786 <fengning65@gmail.com>
2022-11-01 15:40:22 +0800

    install npm as back

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,15 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+const x = 23;
+if (x === 32) {
+  console.log("1111");
+}
+
+const calcAge2 = (birthyear) => 2077 - birthyear;
+console.log(calcAge2(1997));
+
+//第一种就是用liveserver来实现服务器
+//第二种可以使用Nodejs node -v 查看版本
+// npm install live-server
+//安装npm liveserver → npm install live-server-g
