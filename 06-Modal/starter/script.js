0e8bbcc206da4f4af034b65174523e16ecf8e517
CSE2023786 <fengning65@gmail.com>
2022-11-07 17:04:26 +0800

    update 2022/11/7

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,52 @@
 'use strict';
+//这一章的主要目的其实也是为了学习怎么创建类
+//选择需要的元素 并且将他储存
+
+const moKuai = document.querySelector('.modal');
+const fuGai = document.querySelector('.overlay');
+const ButtonClose = document.querySelector('.close-modal');
+const ButtonOpen = document.querySelectorAll('.show-modal'); //注意这里选了所有的模块
+
+//对这3个类进行遍历
+// for(let i=0;i<=ButtonOpen.length-1;i++)
+//对于遍历的这个数据 后面添加事件监听
+// ButtonOpen[i].addEventListener('click',function(){
+//     //应该在这里 添加显示窗口的代码
+//     console.log('按钮点击了这里');
+//     //选择窗口元素 这里用了一个新的classList.remove API 需要注意
+//     //并且 选择类的时候不需要加上.逗号
+//     moKuai.classList.remove('hidden');
+//     fuGai.classList.remove('hidden');
+
+// });
+
+//写一个函数实现效率化 也便于理解
+const openModal = function () {
+  moKuai.classList.remove('hidden');
+  fuGai.classList.remove('hidden');
+};
+const closeModal = function () {
+  moKuai.classList.add('hidden');
+  fuGai.classList.add('hidden');
+};
+
+for (let i = 0; i <= ButtonOpen.length - 1; i++) {
+  ButtonOpen[i].addEventListener('click', openModal);
+}
+
+//现在添加隐藏事件
+ButtonClose.addEventListener('click', closeModal);
+//给点击覆盖层的时候添加关闭功能
+fuGai.addEventListener('click', closeModal);
+
+//还有一种关闭键盘的方法 使用箭头来控制 keydown的时候算三个
+//此时的e 是一个形参
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+  //同时 按Esc键的时候可以退出窗口 如何实现
+  if (e.key === 'Escape') {
+    if (!moKuai.classList.contains('hidden')) {
+      closeModal();
+    }
+  }
+});
--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,42 @@
 'use strict';
+//这个游戏的规则就是 扔骰子
+//扔到非1的数据 加分到100就赢
+
+
+//step1 让这个分数归零 同时让骰子消失 这里用了一个#来选择对象 因为有2个score
+//但是也可以getElementById('.score--0')
+const dice = document.querySelector('.dice');
+const fenShu1 = document.querySelector('#score--0');
+const fenShu2 = document.querySelector('#score--1');
+const Sc1 = document.querySelector('#current--0');//分数
+const Sc2 = document.querySelector('#current--1');
+const buttonRoll = document.querySelector('.btn--roll');
+const buttonHold = document.querySelector('.btn-hold');
+
+let currenScore = 0;
+
+document.querySelector('.btn--new').addEventListener('click',function(){
+    //归0
+    fenShu1.textContent = 0;
+    fenShu2.textContent = 0;
+    //让骰子dice消失
+    dice.classList.add('hidden');//使用这一步的时候需要在CSS里创建style 
+});
+
+//加入扔骰子的功能
+buttonRoll.addEventListener('click',function(){
+    //启动扔骰子
+    const shuZi = Math.trunc(Math.random() * 6 + 1);
+    //根据骰子显示的数字 来显示骰子的图像
+    dice.classList.remove('hidden');
+    dice.src = `dice-${shuZi}.png`; //新东西 添加了src的用法
+    //if true 切换到第二个玩家
+    if(Sc1.textContent != 1){
+        currenScore = currenScore + shuZi;
+        Sc1.textContent = currenScore;
+    }else  {
+      currenScore = currenScore + shuZi;
+      Sc2.textContent = shuZi;
+    };
+});
+
