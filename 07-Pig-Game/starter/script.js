e540058da8f44f3a26295cb0a01da38e92be8ad8
purumomo <fengning65@gmail.com>
2022-11-08 16:46:55 +0800

    update 2022/11/18

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -2,41 +2,100 @@
 //这个游戏的规则就是 扔骰子
 //扔到非1的数据 加分到100就赢
 
-
 //step1 让这个分数归零 同时让骰子消失 这里用了一个#来选择对象 因为有2个score
 //但是也可以getElementById('.score--0')
+const player0El = document.querySelector('.player--0');
+const player0E2 = document.querySelector('.player--1');
 const dice = document.querySelector('.dice');
 const fenShu1 = document.querySelector('#score--0');
 const fenShu2 = document.querySelector('#score--1');
-const Sc1 = document.querySelector('#current--0');//分数
+const Sc1 = document.querySelector('#current--0'); //分数
 const Sc2 = document.querySelector('#current--1');
 const buttonRoll = document.querySelector('.btn--roll');
-const buttonHold = document.querySelector('.btn-hold');
+const buttonHold = document.querySelector('.btn--hold');
+const btnNew = document.querySelector('.btn--new');
 
-let currenScore = 0;
+let currentScore, huoyueshuju, playing, dabifen; //这里可以重新声明变量
 
-document.querySelector('.btn--new').addEventListener('click',function(){
-    //归0
-    fenShu1.textContent = 0;
-    fenShu2.textContent = 0;
-    //让骰子dice消失
-    dice.classList.add('hidden');//使用这一步的时候需要在CSS里创建style 
+const chuShiHua = function () {
+  dabifen = [0, 0];
+  currentScore = 0;
+  huoyueshuju = 0;
+  playing = true;
+
+  Sc1.textContent = 0;
+  Sc2.textContent = 0;
+  fenShu1.textContent = 0;
+  fenShu2.textContent = 0;
+  dice.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player0E2.classList.remove('player--winner');
+  player0E2.classList.remove('player--active');
+};
+chuShiHua(); // 加载页面时就可以使用这个函数
+
+//切换玩家
+const qiehuan = function () {
+  document.getElementById(`current--${huoyueshuju}`).textContent = 0;
+  huoyueshuju = huoyueshuju === 0 ? 1 : 0;
+  currentScore = 0;
+  player0El.classList.toggle('player--active'); //新东西 切换class的内容
+  player0E2.classList.toggle('player--active'); //新东西 切换class的内容
+};
+
+//重建游戏 点击按钮时全部归0 并且可以
+document.querySelector('.btn--new').addEventListener('click', function () {
+  //四个数据都归0
+  fenShu1.textContent = 0;
+  fenShu2.textContent = 0;
+  //让骰子dice消失
+  dice.classList.add('hidden'); //使用这一步的时候需要在CSS里创建style
 });
 
 //加入扔骰子的功能
-buttonRoll.addEventListener('click',function(){
+buttonRoll.addEventListener('click', function () {
+  if (playing === true) {
     //启动扔骰子
     const shuZi = Math.trunc(Math.random() * 6 + 1);
     //根据骰子显示的数字 来显示骰子的图像
     dice.classList.remove('hidden');
     dice.src = `dice-${shuZi}.png`; //新东西 添加了src的用法
-    //if true 切换到第二个玩家
-    if(Sc1.textContent != 1){
-        currenScore = currenScore + shuZi;
-        Sc1.textContent = currenScore;
-    }else  {
-      currenScore = currenScore + shuZi;
-      Sc2.textContent = shuZi;
-    };
+    //if true 切换到第二个玩家 这里比较难
+    if (shuZi !== 1) {
+      currentScore = currentScore + shuZi;
+      //用getId来切换玩家
+      document.getElementById(`current--${huoyueshuju}`).textContent =
+        currentScore;
+    } else {
+      //当数字等于1时 1.原来的玩家数字归0 2，切换背景 3.从第二个玩家开始
+      qiehuan();
+    }
+  }
 });
 
+//添加hold功能
+buttonHold.addEventListener('click', function () {
+  if (playing) {
+    //1.添加current分数到全局分数
+    dabifen[huoyueshuju] += currentScore;
+    console.log(dabifen[huoyueshuju]);
+    document.getElementById(`score--${huoyueshuju}`).textContent =
+      dabifen[huoyueshuju];
+    //2.检查分数是否大于100 是的话完成游戏 并且禁止点击按钮用if true 来解决
+    if (dabifen[huoyueshuju] >= 100) {
+      playing = false;
+      document
+        .querySelector(`.player--${huoyueshuju}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${huoyueshuju}`)
+        .classList.remove('player--active');
+    } else {
+      qiehuan();
+    }
+  }
+});
+
+//加入重置功能
+btnNew.addEventListener('click', chuShiHua);
