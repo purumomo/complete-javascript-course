3a83fbbce262958e09d3ffb5f122aa6347fccb9c
purumomo <fengning65@gmail.com>
2022-11-30 18:48:15 +0800

    update 2022/11/30

---
+++ 11-Arrays-Bankist/starter/数组的AT方法.js
@@ -0,0 +1,12 @@
+//at 方法
+const arr = [23,11,64];
+console.log(arr[0])
+console.log(arr.at(0));
+
+//得到数组的最后一个元素
+console.log(arr[arr.length-1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+
+console.log('jjbom'.at(-1));
+
--- 11-Arrays-Bankist/starter/简单的数组方法.js
+++ 11-Arrays-Bankist/starter/简单的数组方法.js
@@ -74,3 +74,36 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+//数组是对象 
+
+let arr = ['a','b','c','d','e'];
+//slice方法 可以提取数组中任意一部分
+console.log(arr.slice(2));//从下标2开始数['c', 'd', 'e']
+console.log(arr.slice(2,4));//['c', 'd']
+console.log(arr.slice(-2));//从右往左数第二个开始往右数['d', 'e']
+console.log(arr.slice(-1));//['e']
+console.log(arr.slice(1,-2));//首先从下标第一个开始取，取到除了从右往左数第二个['b', 'c']
+console.log(arr.slice());//浅拷贝 ['a', 'b', 'c', 'd', 'e']
+console.log([...arr]);//['a', 'b', 'c', 'd', 'e']
+
+//splice拼接方法
+// console.log(arr.splice(2)); //['c', 'd', 'e']
+arr.splice(-1);//删除下标最后一个
+console.log(arr);//['a', 'b', 'c', 'd']
+arr.splice(1,2);//删除下标1，2
+console.log(arr);//['a', 'd'] 提取的元素消失了
+
+//Reverse反向
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j','i','h','g','j'];
+console.log(arr2.reverse());//['j', 'g', 'h', 'i', 'j']
+console.log(arr2);//['j', 'g', 'h', 'i', 'j']; 同时也改变了原来的数组
+
+//Concat 用于连接两个数组
+const letters = arr.concat(arr2);
+console.log(letters)
+console.log([...arr,...arr2])
+
+//Join 
+console.log(letters.join(' - '));//a - b - c - d - e - j - g - h - i - j
+
